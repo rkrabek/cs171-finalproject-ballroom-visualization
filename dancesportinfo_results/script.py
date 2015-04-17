ee9f5fe50684bb88fe78bdcb10a8724564de6512
Robert <robertkrabek@college.harvard.edu>
2015-04-17 16:43:04 -0700

    uploading trying to fix git issue

--- dancesportinfo_results/script.py
+++ dancesportinfo_results/script.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python
 from __future__ import print_function
 import os
+import json
+from pprint import pprint
+
+result = []
+
+
+
+
+#pprint(data)
 for file in os.listdir("../dancesportinfo_results"):
-    if file.endswith(".json"):
-        print(file)
+	if file.endswith(".json"):
+		with open(file) as data_file:
+			result.append(json.load(data_file))
+
+with open('combined.json', 'w') as outfile:
+	json.dump(result, outfile)
 #var allComps = {};
+
+#pprint(data)
+
